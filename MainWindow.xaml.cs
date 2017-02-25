92d9f9c414fb6b97b07518ed6dabeb9c8693f781
Siketyan <siketyan@sikeserver.com>
2017-02-25 12:34:01 +0900

    ウィンドウ開閉アニメーションを追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace QuickCommander
 {
@@ -23,6 +11,19 @@ namespace QuickCommander
         public MainWindow()
         {
             InitializeComponent();
+            Deactivated += (sender, e) => PlayCloseAnimation();
+        }
+
+        public void PlayShowAnimation()
+        {
+            Storyboard sb = FindResource("ShowAnimation") as Storyboard;
+            sb.Begin();
+        }
+
+        public void PlayCloseAnimation()
+        {
+            Storyboard sb = FindResource("CloseAnimation") as Storyboard;
+            sb.Begin();
         }
     }
 }
